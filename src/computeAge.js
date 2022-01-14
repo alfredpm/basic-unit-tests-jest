43537c51dabb719149f506df4f8dcd97d27e87e5
Alfred PM <alfredpermez@gmail.com>
2022-01-14 17:25:02 +0100

    Fix computeAge comparison and test

--- src/computeAge.js
+++ src/computeAge.js
@@ -3,15 +3,15 @@ function computeAge(birthday, currentDate){
     const currYear = currentDate.getFullYear();
     let age = currYear-birthYear;
 
-    const birthMonth= birthday.getMonth()
-    const currMonth=currentDate.getMonth();
-    if (birthMonth>currMonth){
-        age+=1;
+    const birthMonth = birthday.getMonth()
+    const currMonth = currentDate.getMonth();
+    if (birthMonth < currMonth){
+        age += 1;
     } else {
-        const birthDay= birthday.getDay()
-        const currDay=currentDate.getDay();
-        if (birthDay>=currDay){
-            age+=1;
+        const birthDay = birthday.getDay()
+        const currDay = currentDate.getDay();
+        if (birthDay <= currDay){
+            age += 1;
         }
     }
     return age;
--- test/computeAge.test.js
+++ test/computeAge.test.js
@@ -3,6 +3,6 @@ const computeAge = require("../src/computeAge")
 test ('Né dans un autre mois', () => {
     let birthday = new Date(2000, 0);
     let currentday = new Date(2022, 1);
-    expect(computeAge(birthday,currentday)).toBe(22);
+    expect(computeAge(birthday,currentday)).toBe(23);
 })
 
