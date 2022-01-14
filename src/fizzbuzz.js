25e23a7f91c40125de4cc743eedc48a0c8749ca3
Alfred PM <alfredpermez@gmail.com>
2022-01-14 16:32:34 +0100

    Add FizzBuzz function

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,6 +1,13 @@
 function fizzBuzz (n) {
     // TODO: Compléter fizzbuzz()
-    return "fizzbuzz"
+    let result = new String("");
+    if (n%3==0) {
+        result+="Fizz";
+    }
+    if (n%5==0) {
+        result+="Buzz";
+    }
+    return result;
 }
 
 module.exports = fizzBuzz
--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -2,4 +2,5 @@ const fizzBuzz = require("../src/fizzbuzz")
 
 test('TODO', () => {
     // TODO: Tester fizzbuzz()
+
 })
