e90e55ffcf342987dfee1507cd55b2db2013c28c
Alfred PM <alfredpermez@gmail.com>
2022-01-14 16:39:07 +0100

    Add first tests

--- test/fizzbuzz.test.js
+++ test/fizzbuzz.test.js
@@ -1,6 +1,18 @@
 const fizzBuzz = require("../src/fizzbuzz")
 
-test('TODO', () => {
-    // TODO: Tester fizzbuzz()
-
+test('n divisible par 3', () => {
+    let n = 6;
+    expect(fizzBuzz(n)).toBe("Fizz");
+})
+test('n divisible par 5', () => {
+    let n = 10;
+    expect(fizzBuzz(n)).toBe("Buzz");
+})
+test('n divisible par 3 et 5', () => {
+    let n = 15;
+    expect(fizzBuzz(n)).toBe("FizzBuzz");
+})
+test('n NON divisible par 3 et 5', () => {
+    let n = 2;
+    expect(fizzBuzz(n)).toBe("");
 })
