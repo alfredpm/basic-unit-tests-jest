f200afee1d7a0dee0c04308ab1ce0a317d9cf3e4
Alfred PM <alfredpermez@gmail.com>
2022-01-14 18:03:31 +0100

    Fix computeAge month equality and base age

--- src/computeAge.js
+++ src/computeAge.js
@@ -1,15 +1,15 @@
 function computeAge(birthday, currentDate){
     const birthYear = birthday.getFullYear();
     const currYear = currentDate.getFullYear();
-    let age = currYear-birthYear;
+    let age = currYear-birthYear-1;
 
     const birthMonth = birthday.getMonth()
     const currMonth = currentDate.getMonth();
     if (birthMonth < currMonth){
         age += 1;
-    } else {
-        const birthDay = birthday.getDay()
-        const currDay = currentDate.getDay();
+    } else if (birthMonth==currMonth) {
+        const birthDay = birthday.getDate()
+        const currDay = currentDate.getDate();
         if (birthDay <= currDay){
             age += 1;
         }
