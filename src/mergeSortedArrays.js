eb02ad4f539b9017235499dece66f05123f5f68a
alfredPM <alfredpermez@gmail.com>
2022-01-28 14:58:03 +0100

    Fix function MSArr + test HappyPath

--- src/mergeSortedArrays.js
+++ src/mergeSortedArrays.js
@@ -4,14 +4,24 @@ function mergeSortedArrays(t1,t2){
     let id2=0;
     let tres=[];
     while (i<(t1.length+t2.length)){
-        if (t1[id1]<t2[id2]){
-            tres.push(t1[id1]);
+        //if t1 or t2 entirely treated, add other
+        if (id1>=t1.length){
+            tres.push(t2[id2])
+            id2+=1;
+        } else if (id2>=t2.length) {
+            tres.push(t1[id1])
             id1+=1;
         } else {
-            tres.push(t2[id2]);
-            id2+=1;
+        //if value t2 < value t1 add value t2 at the end, else add value t1
+            if (t2[id2]<t1[id1]){
+                tres.push(t2[id2]);
+                id2+=1;
+            } else {
+                tres.push(t1[id1]);
+                id1+=1;
+            }
         }
-        i=+1;
+        i+=1;
     }
     return tres;
 }
