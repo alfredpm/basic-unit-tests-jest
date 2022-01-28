00168416c135f4a286986f0596d448dd652ecd0a
alfredPM <alfredpermez@gmail.com>
2022-01-28 15:09:15 +0100

    Simplify MSArr

--- src/mergeSortedArrays.js
+++ src/mergeSortedArrays.js
@@ -1,27 +1,18 @@
 function mergeSortedArrays(t1,t2){
-    let i=0;
-    let id1=0;
-    let id2=0;
     let tres=[];
-    while (i<(t1.length+t2.length)){
-        //if t1 or t2 entirely treated, add other
-        if (id1>=t1.length){
-            tres.push(t2[id2])
-            id2+=1;
-        } else if (id2>=t2.length) {
-            tres.push(t1[id1])
-            id1+=1;
+    while (t1.length+t2.length>0){
+        //if t1 empty, add t2
+        if (t1.length==0) {
+            tres.push(t2.shift());
+        } else if (t2.length==0){
+            tres.push(t1.shift());
         } else {
-        //if value t2 < value t1 add value t2 at the end, else add value t1
-            if (t2[id2]<t1[id1]){
-                tres.push(t2[id2]);
-                id2+=1;
+            if (t2[0]<t1[0]){
+                tres.push(t2.shift());
             } else {
-                tres.push(t1[id1]);
-                id1+=1;
+                tres.push(t1.shift())
             }
         }
-        i+=1;
     }
     return tres;
 }
--- test/mergeSortedArrays.test.js
+++ test/mergeSortedArrays.test.js
@@ -3,7 +3,5 @@ const mergeSortedArrays = require("../src/mergeSortedArrays")
 test('correctly merges ordered arrays', () => {
     let t1 = [1,3,4];
     let t2 = [2,5];
-    let tres = mergeSortedArrays(t1,t2);
-    let texp = [1,2,3,4,5];
-    expect(tres).toEqual([1,2,3,4,5]);
+    expect(mergeSortedArrays(t1,t2)).toEqual([1,2,3,4,5]);
 })
