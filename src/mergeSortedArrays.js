7541ba2126857ecf92c614875608505ccc633552
alfredPM <alfredpermez@gmail.com>
2022-01-28 15:36:40 +0100

    Add type check to MSArr

--- src/mergeSortedArrays.js
+++ src/mergeSortedArrays.js
@@ -1,4 +1,18 @@
 function mergeSortedArrays(t1,t2){
+    //Check parameters are ordered arrays
+    if (!Array.isArray(t1)){
+        throw 't1 is not an array'
+    }
+    if (!Array.isArray(t2)){
+        throw 't1 is not an array'
+    }
+    if (!isOrdered(t1)){
+        throw 't1 is not ordered'
+    }
+    if (!isOrdered(t2)){
+        throw 't2 is not ordered'
+    }
+
     let tres=[];
     while (t1.length+t2.length>0){
         //if t1 empty, add t2
@@ -17,4 +31,14 @@ function mergeSortedArrays(t1,t2){
     return tres;
 }
 
+function isOrdered(table){
+    let array = table.slice();
+    while (array.length>0){
+        let curr=array.shift();
+        if (curr>array[0]){
+            return false;
+        }
+    }
+    return true;
+}
 module.exports = mergeSortedArrays
--- test/mergeSortedArrays.test.js
+++ test/mergeSortedArrays.test.js
@@ -3,5 +3,11 @@ const mergeSortedArrays = require("../src/mergeSortedArrays")
 test('correctly merges ordered arrays', () => {
     let t1 = [1,3,4];
     let t2 = [2,5];
-    expect(mergeSortedArrays(t1,t2)).toEqual([1,2,3,4,5]);
+    let res;
+    try {
+        res=mergeSortedArrays(t1,t2);
+    } catch (e) {
+        res=e;
+    }
+    expect(res).toEqual([1,2,3,4,5]);
 })
